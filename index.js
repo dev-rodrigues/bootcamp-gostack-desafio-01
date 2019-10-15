cdcc53c9065189c428f92916ec6705268e571b01
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-10-14 22:23:41 -0300

    buscando projeto por id

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ server.get('/projects', (req, res) => {
     return res.json(projects);
 });
 
+// Deve retonar o projeto do id especificado na URL
+server.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    var project = projects.find(function(element) {
+        return element.id === id;
+    });
+
+    return res.json(project);
+});
+
 server.listen(3000);
